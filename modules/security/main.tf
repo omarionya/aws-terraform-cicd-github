5b6698112939c8b81d47f35b1dbd40e354eabeb6
omarionya <marionya@gmail.com>
2026-02-17 09:44:48 +0300

    tighten security

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -6,20 +6,20 @@ resource "aws_security_group" "web_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["102.208.164.114/32"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["102.208.164.114/32"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["102.208.164.114/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
