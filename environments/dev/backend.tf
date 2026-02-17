db8b8e07057dbe255c55d6bbc8f88c4ae851679a
omarionya <marionya@gmail.com>
2026-02-17 13:10:11 +0300

    more debugging

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,11 +1,15 @@
 terraform {
     required_version = ">= 1.5.0"
 
-  backend "s3" {
-    bucket         = "your-bucket-name"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "your-dynamodb-table-name"
-    encrypt        = true
-  }
+    backend "local" {
+      path = "terraform.tfstate"
+    }
+
+  # backend "s3" {
+  #   bucket         = "your-bucket-name"
+  #   key            = "dev/terraform.tfstate"
+  #   region         = "us-east-1"
+  #   dynamodb_table = "your-dynamodb-table-name"
+  #   encrypt        = true
+  # }
 }
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,10 +1,14 @@
 terraform {
     required_version = ">= 1.5.0"
-  backend "s3" {
-    bucket         = "your-bucket-name"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "your-dynamodb-table-name"
-    encrypt        = true
-  }
+
+    backend "local" {
+      path = "terraform.tfstate"
+    }
+  # backend "s3" {
+  #   bucket         = "your-bucket-name"
+  #   key            = "prod/terraform.tfstate"
+  #   region         = "us-east-1"
+  #   dynamodb_table = "your-dynamodb-table-name"
+  #   encrypt        = true
+  # }
 }
