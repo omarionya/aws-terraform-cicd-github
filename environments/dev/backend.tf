911aebb059bc56202ba0698976c0de2a23e02183
omarionya <marionya@gmail.com>
2026-02-17 08:41:49 +0300

    changes to backend.tf files

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,4 +1,6 @@
 terraform {
+    required_version = ">= 1.5.0"
+
   backend "s3" {
     bucket         = "enterprise-terraform-state-ruiru"
     key            = "dev/terraform.tfstate"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,4 +1,5 @@
 terraform {
+    required_version = ">= 1.5.0"
   backend "s3" {
     bucket         = "enterprise-terraform-state-ruiru"
     key            = "prod/terraform.tfstate"
